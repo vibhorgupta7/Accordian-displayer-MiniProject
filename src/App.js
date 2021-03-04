55970392d40d37890e6ff732b90d9b152ae60db8
vibhorgupta7 <vibhor4k@gmail.com>
2021-03-04 11:19:28 +0530

    App component done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { useState } from 'react';
+import { Manager } from 'react-popper';
 import data from './data';
 import SingleQuestion from './Question';
+
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions,setQuestions] = useState(data);
+
+
+  return(
+    <main>
+      <div className="container">
+          <h3>Questions and Answers about Login. </h3>
+
+          <section className="info">
+              {questions.map((question)=> {
+                return <SingleQuestion key={question.id} {...questions}/>
+              }
+              )}
+          </section>
+      </div>
+    </main>
+  )
 }
 
 export default App;
